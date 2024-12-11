d172defa6febeccc8755e86514b1a6f0bdd749a0
ffturan <fehmifturan@gmail.com>
2024-12-11 12:53:51 -0500

    Update

--- container/main_tiff_re.py
+++ container/main_tiff_re.py
@@ -30,7 +30,7 @@ sns_topic_arn = os.environ['SNS_TOPIC_ARN']
 def detect_ssn(text):
     
     # WORKS FINE
-    ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!666|000|[9..])\d{3}-\d{2}-\d{4})|(?!666|000|[9..])\d{9}|((?!666|000|[9..]])\d{3}\s\d{2}\s\d{4})'
+    ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!666|000|[9..])\d{3}-\d{2}-\d{4})|(?!666|000|[9..])\d{9}|((?!666|000|[9..])\d{3}\s\d{2}\s\d{4})'
     # ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!666|000|9\\d{2})\d{3}-\d{2}-\d{4})|((?!666|000|9\\d{2})\d{9})|((?!666|000|9\\d{2})\d{3}\s\d{2}\s\d{4})'
     # WORKS FINE [ALTERNATIVE 1]
     # ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|(\d{3}-\d{2}-\d{4})|(\d{9})|(\d{3}\s\d{2}\s\d{4})'
