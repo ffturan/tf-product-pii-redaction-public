708a4ede7cc9cd0bf953f82c76f6b6668ec20030
ffturan <fehmifturan@gmail.com>
2024-12-12 11:27:29 -0500

    Update

--- container/main_tiff_re.py
+++ container/main_tiff_re.py
@@ -30,17 +30,8 @@ sns_topic_arn = os.environ['SNS_TOPIC_ARN']
 def detect_ssn(text):
     
     # WORKS FINE
-    ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!666|000|[9..])\d{3}-\d{2}-\d{4})|((?!666|000|[9..])\d{3}\s\d{2}\s\d{4})|^(?!000|666|9\d{8}$)\d{9}$'
-    # ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!666|000|9\\d{2})\d{3}-\d{2}-\d{4})|((?!666|000|9\\d{2})\d{9})|((?!666|000|9\\d{2})\d{3}\s\d{2}\s\d{4})'
-    # WORKS FINE [ALTERNATIVE 1]
-    # ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|(\d{3}-\d{2}-\d{4})|(\d{9})|(\d{3}\s\d{2}\s\d{4})'
-    # OPTIONAL : r'^(?:AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4}|(?:\d{3}-\d{2}-\d{4}|\d{3}\s\d{2}\s\d{4})(?!666|000|9)|\d{9}$'
-    # NOT THAT GOOD
-    # ssn_pattern = r'([A-Za-z]+&?[A-Za-z]*\d{3}-?\d{2}-?\d{4}|\d{3}-?\d{2}-?\d{4}|\d{9})'
-    # ssn_pattern = r'\b(?!000|666|9\d{2})([0-8]\d{2}|7([0-6]\d|7[012]))([-\s]?)(?!00)\d{2}\3(?!0000)\d{4}\b'
-    # ssn_pattern = r'\b([A-Za-z]{1}(&?)[a-zA-Z]{1})?(?=[-\s]?\d{3}[-\s]?\d{2}[-\s]?\d{4}\b)[-\s]?\d{3}[-\s]?\d{2}[-\s]?\d{4}\b'
-    # ssn_pattern = r'([A-Za-z]+&?[A-Za-z]*\s?\d{3}\s?-?\s?\d{2}\s?-?\s?\d{4}|\d{3}\s?-?\s?\d{2}\s?-?\s?\d{4}|\d{3}\s?\d{2}\s?\d{4})'
-    
+    ssn_pattern = r'(^(AP|A&P|NC|NX|NR|NL)\d{3}-?\s?\d{2}-?\s?\d{4})|((?!000|666|[9..])\d{3}-\d{2}-\d{4})|((?!000|666|[9..])\d{3}\s\d{2}\s\d{4})|^(?!000|666|[9..]])\d{9}$'
+        
     matches = []
     for match in re.finditer(ssn_pattern, text):
         matched_text = match.group()  # Get the actual matched text
